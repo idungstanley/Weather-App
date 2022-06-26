62a23c025ce5ab21a36d5b81294cb02ef0b54bda
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-26 18:13:07 +0100

    Add link

--- src/Component/Nav/Navbar.jsx
+++ src/Component/Nav/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css';
+import logo from '../../Assets/weather-logo.png';
 
 const Navbar = () => (
   <div className="container">
-    <h1>Country Population</h1>
+    <img src={logo} alt="logo" />
     <ul className="link-container">
       <li className="link">
         <Link to="/">Home</Link>
