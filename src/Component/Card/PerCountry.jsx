cfe4b46fc25ace9b694549c982e5bc5219464b66
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-27 10:40:07 +0100

    Add build

--- src/Component/Card/PerCountry.jsx
+++ src/Component/Card/PerCountry.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { BsFillArrowRightCircleFill } from 'react-icons/bs'
-import useRegion from '../../hooks/useRegion'
-import { getDetails } from '../../redux/Polution'
-import './card.css'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import useRegion from '../../hooks/useRegion';
+import { getDetails } from '../../redux/Polution';
+import './card.css';
 
 const PerCountry = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { region } = useRegion()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { region } = useRegion();
   const handleClick = (lat, lon) => {
-    dispatch(getDetails(lat, lon))
-    navigate('/Countries')
-  }
+    dispatch(getDetails(lat, lon));
+    navigate('/Countries');
+  };
   return region.map(
     ({
       id,
@@ -27,52 +27,40 @@ const PerCountry = () => {
       lat,
       lon,
     }) => (
-      <article className='card' key={id}>
-        <div className='firstSection'>
-          <p className='countryName'>{countryName}</p>
-          <div className='fg'>
+      <article className="card" key={id}>
+        <div className="firstSection">
+          <p className="countryName">{countryName}</p>
+          <div className="fg">
             <img
               src={coatOfArms === undefined ? flags : coatOfArms}
-              alt=''
-              className='coa'
+              alt=""
+              className="coa"
             />
           </div>
         </div>
-        <div className='secondSection'>
-          <div className='details'>
+        <div className="secondSection">
+          <div className="details">
             <BsFillArrowRightCircleFill
               style={{ cursor: 'pointer', color: '#00A1E5' }}
               size={30}
               onClick={() => handleClick(lat, lon)}
             />
-            <p className='font'>
+            <p className="font">
               Population:
               {population}
             </p>
-            <p className='font'>{timezones}</p>
-            <p className='font'>{continents}</p>
-            <button type='button'>
-              <a href={maps} target='_blank' rel='noreferrer' className='map'>
+            <p className="font">{timezones}</p>
+            <p className="font">{continents}</p>
+            <button type="button">
+              <a href={maps} target="_blank" rel="noreferrer" className="map">
                 View map
               </a>
             </button>
           </div>
-          <img src={flags} alt='' className='flags' />
+          <img src={flags} alt="" className="flags" />
         </div>
       </article>
-    )
-  )
-}
-
-// PerCountry.propTypes = {
-//   countryName: PropTypes.string.isRequired,
-//   population: PropTypes.string.isRequired,
-//   maps: PropTypes.string.isRequired,
-//   timezones: PropTypes.string.isRequired,
-//   coatOfArms: PropTypes.string.isRequired,
-//   continents: PropTypes.string.isRequired,
-//   flags: PropTypes.string.isRequired,
-//   lat: PropTypes.number.isRequired,
-//   lon: PropTypes.number.isRequired,
-// };
-export default PerCountry
+    ),
+  );
+};
+export default PerCountry;
