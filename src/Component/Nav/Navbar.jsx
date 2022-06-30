3460b787a0359d8dc03c573c9c20e3422abd8ae4
sunday stanley Idung <sundaystanley56@gmail.com>
2022-06-30 22:08:42 +0100

    Updated my read.me

--- src/Component/Nav/Navbar.jsx
+++ src/Component/Nav/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Divide as Hamburger } from 'hamburger-react';
 import './nav.css';
 import logo from '../../Assets/weather-logo.png';
 
 const Navbar = () => (
   <div className="container">
     <img src={logo} alt="logo" />
-    <ul className="link-container">
-      <li className="link">
-        <Link to="/">Home</Link>
-      </li>
-    </ul>
+    <Hamburger onToggle={(toggled) => {
+      if (toggled) {
+        <ul className="link-container">
+          <li className="link">
+            <Link to="/">Home</Link>
+          </li>
+        </ul>;
+      }
+    }}
+    />
   </div>
 
 );
